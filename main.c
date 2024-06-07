4b71753b9d6ac5bbd6f6dd3519a884905445f0dd
Tamilly <116323270+ctamilly@users.noreply.github.com>
2024-06-06 23:27:45 -0300

    add função strcasecmp
    Adiciona função auxiliar para comparar strings ignorando maiúsculas e minúsculas

--- main.c
+++ main.c
@@ -3,3 +3,16 @@
 #include <string.h>
 #include <ctype.h>
 #include "playlist.h"
+
+int strcasecmp(const char *s1, const char *s2) {
+    while (*s1 && *s2) {
+        char c1 = tolower((unsigned char) *s1);
+        char c2 = tolower((unsigned char) *s2);
+        if (c1 != c2) {
+            return c1 - c2;
+        }
+        s1++;
+        s2++;
+    }
+    return tolower((unsigned char) *s1) - tolower((unsigned char) *s2);
+}
